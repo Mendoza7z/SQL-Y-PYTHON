3b4e75b5460db3653431fdc9ed7621fda460cc39
Mendoza7z <lomendoza@scl.edu.gt>
2025-03-10 20:49:29 -0600

    Delete inventario directory

--- inventario/main.py
+++
@@ -1,4 +0,0 @@
-from modes.menu import ejecutar_menu
-
-if __name__ == "__main__":
-    ejecutar_menu()
--- inventario/services/inventario.py
+++
@@ -1,7 +0,0 @@
-productos = [
-    {'nombre': 'Limones', 'cantidad': 50, 'precio': 2},
-    {'nombre': 'Naranjas', 'cantidad': 30, 'precio': 5},
-    {'nombre': 'Leche', 'cantidad': 20, 'precio': 7.50},
-    {'nombre': 'Pan', 'cantidad': 15, 'precio': 4},
-    {'nombre': 'Huevos', 'cantidad': 12, 'precio': 0.50}
-]
